a0849d375d6ede2935cc81847d41fc3f73970b91
Kauan Alves <kauan.alves1996@hotmail.com>
2018-09-08 14:46:22 -0300

    Alterando inserções iniciais para inserir os quartos no banco de dados

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 """Modulo para criar os comandos de inicialização da aplicação."""
 from flask_script import Manager
 from sistema_hotel import app
-from sistema_hotel.models.insertions import insert_users
+from sistema_hotel.models.insertions import insert_users, insert_rooms
 
 manager = Manager(app)
 
@@ -14,6 +14,7 @@ def runserver():
 @manager.command
 def insertions():
     insert_users()
+    insert_rooms()
 
 
 if __name__ == '__main__':
--- sistema_hotel/models/insertions.py
+++ sistema_hotel/models/insertions.py
@@ -1,4 +1,4 @@
-from sistema_hotel.models.tables import session, User
+from sistema_hotel.models.tables import session, User, Room
 
 
 def insert_users():
@@ -20,4 +20,38 @@ def insert_users():
         activity=False
     )
     session.add(hospede01)
+    session.commit()
+
+def insert_rooms():
+    room1 = Room(
+        number=10,
+        floor=1,
+        description="Quarto do primeiro andar",
+        daily_value=89.90,
+        status='Livre'
+    )
+
+    session.add(room1)
+    session.commit()
+
+    room2 = Room(
+        number=20,
+        floor=1,
+        description="Quarto do primeiro andar",
+        daily_value=89.90,
+        status='Livre'
+    )
+
+    session.add(room2)
+    session.commit()
+
+    room3 = Room(
+        number=30,
+        floor=1,
+        description="Quarto do primeiro andar",
+        daily_value=89.90,
+        status='Livre'
+    )
+
+    session.add(room3)
     session.commit()
