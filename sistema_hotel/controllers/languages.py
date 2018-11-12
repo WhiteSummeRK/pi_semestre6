30fc30b67e10f9b61fd2a733e057d1d4db7557c7
Kauan Alves <kauan.alves1996@hotmail.com>
2018-11-12 15:44:38 -0200

    adicionando multi-linguagem ao menu principal

--- sistema_hotel/controllers/languages.py
+++ sistema_hotel/controllers/languages.py
@@ -1,19 +1,40 @@
 messages = {
     'pt-br': {
+        'leave': 'Sair',
         'menu_title': 'Menu - Hotel Service',
         'sidebar_users': 'Usuários',
         'sidebar_residents': 'Hóspedes',
         'sidebar_services': 'Serviços',
-        'sidebar_rooms': 'Quartos'
+        'sidebar_rooms': 'Quartos',
+        'menu_new_user_msg': 'Aqui você pode cadastrar um novo usuario, este pode vir a ser um Hospede ou ADM',
+        'menu_new_resident_msg': 'Se voce já cadastrou um usuário, pode transforma-lo em um novo hóspede',
+        'menu_new_service': 'Vizualize aqui os pedidos efetuados a cozinha',
+        'menu_new_room': 'Vizualize os quartos e seus estados',
+        'menu_checkin_msg': 'Aqui você pode fazer o Check-in de hóspedes já cadastrados',
+        'menu_checkout_msg': 'Aqui você pode fazer o Check-out ao fim da estadia de um hóspede',
+        'menu_': '',
+        'menu_': '',
+        'menu_': '',
+
 
 
     },
     'en': {
+        'leave': 'Exit',
         'menu_title': 'Menu - Hotel Service',
         'sidebar_users': 'Users',
         'sidebar_residents': 'Residents',
         'sidebar_services': 'Services',
-        'sidebar_rooms': 'Rooms'
-
+        'sidebar_rooms': 'Rooms',
+        'menu_new_user_msg': 'Here you can registrate new users, ADM or resident',
+        'menu_new_resident_msg': 'If you already registered a user, it can become a new resident',
+        'menu_new_service': 'View here the requests made to the kitchen',
+        'menu_new_room': 'View here the rooms and their states',
+        'menu_checkin_msg': 'Here you can check in already registered residents',
+        'menu_checkout_msg': "Here you can check out at the end of a guest's stay",
+        'menu_': '',
+        'menu_': '',
+        'menu_': '',
+        'menu_': '',
     }
 }
